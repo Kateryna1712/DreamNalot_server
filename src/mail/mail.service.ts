921969bd47dd1c94b4d957548758476250dc3682
Kateryna1712 <katrinmatyash01@gmail.com>
2024-09-25 11:37:33 +0300

    some fix with email

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -2,46 +2,44 @@ import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 import { SendMailDto, MailDto } from './dto/SendMailDto';
 import { Model } from 'mongoose';
-import { Mail, MailDocument } from './mail.schema'; 
+import { Mail, MailDocument } from './mail.schema';
 import { InjectModel } from '@nestjs/mongoose';
-
 
 @Injectable()
 export class MailService {
-    constructor(private readonly mailerService:MailerService,
-        @InjectModel(Mail.name) private mailRepository: Model<MailDocument>
-    ){}
-    
-    sendMail(dto: SendMailDto){
-        this.mailerService.sendMail({
-            to:'katrinmatyash01@gmail.com',
-            from:process.env.EMAIL,
-            subject:'Message',
-            text:`${dto.service}`,
-            html:`<body>
+  constructor(
+    private readonly mailerService: MailerService,
+    @InjectModel(Mail.name) private mailRepository: Model<MailDocument>,
+  ) {}
+
+  sendMail(dto: SendMailDto) {
+    this.mailerService.sendMail({
+      to: process.env.EMAIL_RECEIVER,
+      from: process.env.EMAIL,
+      subject: 'Message',
+      text: `${dto.service}`,
+      html: `<body>
             <h1>${dto.name}</h1>
             <h2>${dto.email}</dto>
             <h2>${dto.phoneNumber}</h2>
             <div>
             ${dto.comment}
             </div>
-            </body>`
-        });
-    }
+            </body>`,
+    });
+  }
 
-    async saveMailToDB(email: string){
-        console.log(email)
-        try{
-            const user=await this.mailRepository.create({
-                email:email
-            });
-            return user;
-        }
-        catch(e){
-            console.log(e);
+  async saveMailToDB(email: string) {
+    console.log(email);
+    try {
+      const user = await this.mailRepository.create({
+        email: email,
+      });
+      return user;
+    } catch (e) {
+      console.log(e);
 
-            throw e;
-        }
-        
+      throw e;
     }
+  }
 }
